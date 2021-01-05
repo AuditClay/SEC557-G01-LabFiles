2a4d605d603b567d3f5c70355558a1d1abf369e1
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-01-04 19:07:49 -0600

    Update GetPatchData.ps1

--- Lab3.1/GetPatchData.ps1
+++ Lab3.1/GetPatchData.ps1
@@ -1,6 +1,15 @@
-﻿Get-Content .\patchInput.csv |
+﻿  Get-Content .\patchInput.csv |
   ConvertFrom-Csv |
   Select-Object Source, ` 
     @{N='InstalledOn';E={(Get-Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0).AddDays($_.DateOffset)}}, `
     Description, InstalledBy |Where-Object InstalledOn -lt (Get-Date) |
   Export-Csv -Force -NoTypeInformation -Path patches.csv
+
+  Get-Content .\patchAgeInput.csv |
+  ConvertFrom-Csv |
+  Select-Object ServerName, ` 
+    @{N='Date';E={(Get-Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0).AddDays($_.DateOffset)}}, `
+    PatchAge |Where-Object Date -lt (Get-Date) |
+  Export-Csv -Force -NoTypeInformation -Path patchAge.csv
+  
+ 
