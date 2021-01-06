dc5cdc705119acbc9df08579d02f941b57daf60c
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-01-05 20:17:23 -0600

    Update GetPatchData.ps1

--- PoshWebcasts/Webcast1/GetPatchData.ps1
+++ PoshWebcasts/Webcast1/GetPatchData.ps1
@@ -1,4 +1,7 @@
-﻿Get-Content .\patchInput.csv |
+﻿"Admins are gathering 1 year of patch data for 100 servers"
+"This may take a minute..."
+
+Get-Content .\patchInput.csv |
   ConvertFrom-Csv |
   Select-Object Source, ` 
     @{N='InstalledOn';E={(Get-Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0).AddDays($_.DateOffset)}}, `
