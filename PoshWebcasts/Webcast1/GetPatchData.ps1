6f987577961907a0bfd2979aff94ab839371f095
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-01-05 20:08:14 -0600

    Update GetPatchData.ps1

--- PoshWebcasts/Webcast1/GetPatchData.ps1
+++ PoshWebcasts/Webcast1/GetPatchData.ps1
@@ -2,7 +2,7 @@
   ConvertFrom-Csv |
   Select-Object Source, ` 
     @{N='InstalledOn';E={(Get-Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0).AddDays($_.DateOffset)}}, `
-    Description, InstalledBy |Where-Object InstalledOn -lt (Get-Date) |
+    HotFixID, Description, InstalledBy |Where-Object InstalledOn -lt (Get-Date) |
   Export-Csv -Force -NoTypeInformation -Path patches.csv
 
   Get-Content .\patchAgeInput.csv |
