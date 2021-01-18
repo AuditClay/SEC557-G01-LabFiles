aa94b47bb358adbf2f89d5a581ae15c65b7cc79b
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-01-17 23:26:54 -0600

    lab files

--- Lab3.2/Windows.Tests.ps1
+++ Lab3.2/Windows.Tests.ps1
@@ -16,11 +16,50 @@ Describe "Windows Workstation Compliance Tests" {
         It "Guest user disabled" {
             $disabledUsers | Should -Contain "Guest"
         }
+        It "Power users group should be empty" {
+            (Get-LocalGroupMember -Name 'Power Users').Count | Should -Be 0
+        }
+    }
+    Context "Windows Registry" {
+        It "LimitBlankPasswordUse is enabled" {
+            (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa").LimitBlankPasswordUse | 
+                Should -Be 1
+        }
+        It "NoLMHash is enabled" {
+            (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa").NoLMHash | 
+                Should -Be 1
+        }
+        It "LimitBlankPasswordUse is enabled" {
+            (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa").RestrictAnonymous | 
+                Should -Be 1
+        }
     }
     Context "Local Security Policy"{
-
+        #Save the local policy as a text file before running the tests in this Context block
+        BeforeAll {
+            SecEdit.exe /export /cfg .\localSecPolicy.txt
+            $localPolicy = Get-Content .\localSecPolicy.txt
+        }
+        #Delete the local policy export file when finished with the tests in this Context block
+        AfterAll {
+            Remove-Item .\localSecPolicy.txt
+        }
+        It "MinimumPasswordAge > 7" {
+            ($localPolicy | Select-String "MinimumPasswordAge" -NoEmphasis) `
+                -Replace 'MinimumPasswordAge = ', '' | 
+                Should -BeGreaterOrEqual 7
+        }
+        It "MaximumPasswordAge > 30" {
+            ($localPolicy | Select-String "MaximumPasswordAge" -NoEmphasis) `
+                -Replace 'MaximumPasswordAge = ', '' | 
+                Should -BeGreaterOrEqual 30
+        }
     }
     Context "Services" {
-
+        It "OSQuery service properly configured" {
+            (Get-Service -Name 'osqueryd').Count | Should -Be 1
+            (Get-Service -Name 'osqueryd').Status | Should -Be 'Running'
+            (Get-Service -Name 'osqueryd').StartType | Should -BeLike 'Automatic*'
+        }
     }
 }
