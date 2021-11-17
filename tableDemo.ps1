26e24ccf9e99d23caae8196e51fc23a482761814
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-11-17 15:22:38 -0600

    lab testing

---
+++ tableDemo.ps1
@@ -0,0 +1,21 @@
+#Get today's date with no time 
+$today = (get-date).ToShortDateString()
+
+#Now, convert it to an epoch time
+$todaySec = Get-Date -date $today -AsUTC -UFormat %s
+
+#Create a predictable "random" number generator so all
+#students get the same results
+Get-Random -SetSeed 314159 | Out-Null
+
+#Create data for 10 servers
+for( $i=0; $i -lt 10; $i++){
+    $hostname = "Server$i"
+    $diskFree = Get-Random -Minimum 0 -Maximum 100
+    $cpuAvg = Get-Random -Minimum 0 -Maximum 100
+    $uptimeDays = Get-Random -Minimum 0 -Maximum 365
+
+    "demo.table.$hostname.diskfree $diskFree $todaySec"
+    "demo.table.$hostname.cpuavg $cpuAvg $todaySec"
+    "demo.table.$hostname.uptimedays $uptimeDays $todaySec"
+}
