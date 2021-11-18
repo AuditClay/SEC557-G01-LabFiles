75cc63155b2828c9654aac8ff691e34174392df3
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-11-17 19:14:18 -0600

    lab testing

--- tableDemo.ps1
+++ tableDemo.ps1
@@ -5,25 +5,35 @@ $today = Get-Date -Format "yyyy-MM-dd"
 #students get the same results
 Get-Random -SetSeed 314159 | Out-Null
 
-"#this script creates a table in MySql to emonstrate using tabular data in Grafana"
+"#this script creates a table in MySql to demonstrate using tabular data in Grafana"
 "create database if not exists grafana;"
 "use grafana;"
 "create table if not exists serverstats( dateRun date, servername varchar(100), diskfree int, cpuavg int, uptime int);"
 "delete from serverstats;"
 
-#Create data for 10 servers
-for( $i=0; $i -lt 10; $i++){
-    $hostname = "Server$i"
-    $diskFree = Get-Random -Minimum 0 -Maximum 100
-    $cpuAvg = Get-Random -Minimum 0 -Maximum 100
-    $uptimeDays = Get-Random -Minimum 0 -Maximum 365
-
-    "insert into serverstats (dateRun, servername, diskfree, cpuavg, uptime) values "
-    "('$today', '$hostname', $diskfree, $cpuAvg, $uptimeDays);"
-    # "sec557.demo.table.$hostname.diskfree $diskFree $todaySec"
-    # "sec557.demo.table.$hostname.cpuavg $cpuAvg $todaySec"
-    # "sec557.demo.table.$hostname.uptimedays $uptimeDays $todaySec"
+#Create data for 10 servers for 10 days
+for( $day = (Get-Date).addDays(-10); $day -le (Get-date); $day = $day.AddDays(1)){
+    for( $i=0; $i -lt 10; $i++){
+        $today = $day.ToShortDateString()
+        $hostname = "Server$i"
+        $diskFree = Get-Random -Minimum 0 -Maximum 100
+        $cpuAvg = Get-Random -Minimum 0 -Maximum 100
+        $uptimeDays = Get-Random -Minimum 0 -Maximum 365
+    
+        "insert into serverstats (dateRun, servername, diskfree, cpuavg, uptime) values "
+        "('$today', '$hostname', $diskfree, $cpuAvg, $uptimeDays);"
+        # "sec557.demo.table.$hostname.diskfree $diskFree $todaySec"
+        # "sec557.demo.table.$hostname.cpuavg $cpuAvg $todaySec"
+        # "sec557.demo.table.$hostname.uptimedays $uptimeDays $todaySec"
+    }
 }
 
 "#data in table is:"
 "select * from serverstats;"
+<#
+SELECT
+  daterun AS "time",
+  servername, diskfree,cpuavg,uptime
+FROM serverstats
+ORDER BY time
+#>
